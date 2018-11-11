ec75c6486351ccc85ae096dba97cc1d8d688f528
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-11 23:09:44 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
+  block_return_values = []
   i = 0
   while i < collection.length
     yield(collection[i])
     i += 1
   end
+  block_return_values
 end
